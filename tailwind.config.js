5f8d3d17e0f610ac9b7595f29179349b8d5038be
rafazzaky <rafiazzakymc@gmail.com>
2023-03-15 09:57:28 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:  {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#84cc16',
+        dark: '#0f172a',
+        light: '#64748b',
+        secondary: '',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
